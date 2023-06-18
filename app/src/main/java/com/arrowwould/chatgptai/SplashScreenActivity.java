dabd04c26069bb04dae0c8d333eb43e740187a99
samyak121 <136222409+samyak121@users.noreply.github.com>
2023-06-18 21:36:02 +0530

    chatgptai.apk

---
+++ app/src/main/java/com/arrowwould/chatgptai/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.arrowwould.chatgptai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Intent iHome = new Intent(SplashScreenActivity.this, MainActivity.class);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(iHome);
+                finish();
+            }
+        }, 4000);
+
+    }
+}
